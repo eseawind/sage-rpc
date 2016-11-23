df3031054439e1b72f02de4e9a44aac5425fe587
sage.xue <job.xueqi@gmail.com>
2016-11-23 23:13:55 +0800

    完成httpinvoke的request

---
+++ sage-rpc-invoke/sage-rpc-invoke-api/src/main/java/org/sagesource/rpc/invoke/exception/RpcInvokeException.java
@@ -0,0 +1,26 @@
+package org.sagesource.rpc.invoke.exception;
+
+import org.sagesource.rpc.core.exception.RpcException;
+
+/**
+ * <p></p>
+ * <pre>
+ *     author      Sage XueQi
+ *     date        2016/11/23
+ *     email       job.xueqi@gmail.com
+ * </pre>
+ */
+public class RpcInvokeException extends RpcException {
+
+	public RpcInvokeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public RpcInvokeException(String message, Throwable cause, Object data) {
+		super(message, cause, data);
+	}
+
+	public RpcInvokeException(String message, Object data) {
+		super(message, data);
+	}
+}
--- sage-rpc-invoke/sage-rpc-invoke-http/src/main/java/org/sagesource/rpc/invoke/http/HttpInvoker.java
+++ sage-rpc-invoke/sage-rpc-invoke-http/src/main/java/org/sagesource/rpc/invoke/http/HttpInvoker.java
@@ -1,6 +1,21 @@
 package org.sagesource.rpc.invoke.http;
 
-import org.sagesource.rpc.invoke.Invoker;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.sagesource.common.Charsets;
+import org.sagesource.common.Constants;
+import org.sagesource.rpc.invoke.exception.RpcInvokeException;
+import org.sagesource.rpc.invoke.http.client.InvokeHttpClient;
+import org.sagesource.rpc.invoke.http.dto.InvokeTargetInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>Http传输层实现</p>
@@ -10,9 +25,49 @@ import org.sagesource.rpc.invoke.Invoker;
  *     email       job.xueqi@gmail.com
  * </pre>
  */
-public class HttpInvoker implements Invoker {
-	@Override
-	public String request() {
-		return null;
+public class HttpInvoker {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HttpInvoker.class);
+
+	/**
+	 * 请求远程服务
+	 *
+	 * @param requestMessage   消息
+	 * @param invokeTargetInfo 远程服务调用信息
+	 * @return
+	 */
+	public String request(String requestMessage, InvokeTargetInfo invokeTargetInfo) throws RpcInvokeException {
+		String   serviceUrl = invokeTargetInfo.builtUrl();
+		HttpPost post       = new HttpPost(serviceUrl);
+		post.setHeader("Connection", "Keep-Alive");
+		List<NameValuePair> params = new ArrayList<>();
+		params.add(new BasicNameValuePair(Constants.SAGE_HTTP_INVOKE_DATA, requestMessage));
+
+		LOGGER.debug("Begin Remote Processor Call,message=[{}],url=[{}]", requestMessage, serviceUrl);
+
+		try {
+			post.setEntity(new UrlEncodedFormEntity(params, Charsets.UTF_8));
+			CloseableHttpResponse response = InvokeHttpClient.getHttpClient(invokeTargetInfo).execute(post);
+
+			if (response != null && response.getStatusLine().getStatusCode() == 200) {
+				return EntityUtils.toString(response.getEntity(), Charsets.UTF_8);
+			} else {
+				throw new RpcInvokeException("Remote Processor Call Response Error", requestMessage + "[" + serviceUrl + "]");
+			}
+		} catch (Exception e) {
+			throw new RpcInvokeException("Remote Processor Call Exception", e, requestMessage + "[" + serviceUrl + "]");
+		}
+	}
+
+	//.......................................................................//
+
+	/**
+	 * 单例类
+	 */
+	private static class SingletonHolder {
+		private static final HttpInvoker INSTANCE = new HttpInvoker();
+	}
+
+	public static HttpInvoker getInstance() {
+		return SingletonHolder.INSTANCE;
 	}
 }
